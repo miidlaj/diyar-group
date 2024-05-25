52af71c61261893d0326396020930cb69b058adc
miidlaj <mumidlaj@gmail.com>
2024-05-25 09:20:44 +0530

    reservation check in page and tentative list

--- src/pages/reservation/check-in/page.tsx
+++ src/pages/reservation/check-in/page.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import { FunctionComponent } from "react";
 import { CloudDownload, Edit, Plus, Scroll, Search } from "lucide-react";
 import {
   Table,
